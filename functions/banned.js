c55caabd7b9bb7565ae1d38fe5a60b7dd656c6dd
Emil Barranco <41457874+EmilBInv@users.noreply.github.com>
2018-08-18 14:05:09 -0400

    Again Banned

--- functions/banned.js
+++ functions/banned.js
@@ -1,12 +1,17 @@
 const Discord = require('discord.js');
 exports.run = (AI, message, args) => {
-  let blackList = ['fuck', 'bitch', 'slut'];
-  let foundInText = false;
-  for(var i in blackList) {
-    if(message.content.toLowerCase().includes(toLowerCase())) foundInText = true;
-  }
-  if(foundInText) {
-    message.delete();
-    message.channel.send("Hey! That word is not allowed here... :neutal_face:");
+  AI.on('message', async (message) => {
+    let blackList = ['fuck', 'bitch', 'slut'];
+    let foundInText = false;
+    for(var i in blackList) {
+      if(message.content.toLowerCase().includes(toLowerCase())) foundInText = true;
+    }
+    if(foundInText) {
+      message.delete();
+      let embed = new Discord.RichEmbed()
+      .addField("Hey! That word is not allowed here... :neutal_face:")
+      .setColor("#7429AA")
+      .setTimestamp()
+      return await message.channel.send({embed});
   }
 }
