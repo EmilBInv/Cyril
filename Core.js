bc249ba6faa70cf2917bba69ea2d7df45be69800
Emil Barranco <41457874+EmilBInv@users.noreply.github.com>
2018-08-18 15:09:53 -0400

    added a nairobi

--- Core.js
+++ Core.js
@@ -30,7 +30,7 @@ AI.on("message", async (message) => {
    const command = args.shift().toLowerCase();
      
    //-----------------Banned Words-----------------
-const args = message.content.();
+const args = message.content();
 const swear = args.shift().toLowerCase();
  if (swear === 'nairobi') {
       message.delete()
