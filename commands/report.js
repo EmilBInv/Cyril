605077f307dfbf5014296cc9b9f41bff462e8843
Emil Barranco <41457874+EmilBInv@users.noreply.github.com>
2018-08-19 10:17:05 -0400

    Gifs

--- commands/report.js
+++ commands/report.js
@@ -8,10 +8,11 @@ exports.run = (AI, message, args) => {
   let reportEmbed = new Discord.RichEmbed()
     .setDescription("Report")
     .setColor("#2E86C1")
-    .addField(":anger: Reported User", `${rUser}`)
-    .addField(":speaking_head: Reported By", `${message.author}`)
-    .addField(":exclamation: Reason", repReason)
-    .addField(":closed_book: Channel", message.channel)
+    .addField(":anger: Reported User:", `${rUser}`)
+    .addField(":speaking_head: Reported By:", `${message.author}`)
+    .addField(":exclamation: Reason:", repReason)
+    .addField(":closed_book: Channel:", message.channel)
+    .setThumbnail("https://media.giphy.com/media/3SZUsUsZvSpJm/giphy.gif")
     .setTimestamp();
 
   let reportsChannel = message.guild.channels.find(`name`, "reports");
