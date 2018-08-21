091621bb2dd790833a43e03286dc1c418e9a6ff7
Emil Barranco <41457874+EmilBInv@users.noreply.github.com>
2018-08-21 11:55:03 -0400

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 exports.run = (AI, message, args) => {
 const info = new Discord.RichEmbed()
-.setAuthor("Asher | Emil Barranco", "https://cdn.glitch.com/22f8ea41-6014-45f6-a164-9267d55b17e5%2FGreyCover.jpg?1534656385652")
+.setAuthor("Rehsa | Asher", "https://cdn.glitch.com/22f8ea41-6014-45f6-a164-9267d55b17e5%2FGreyCover.jpg?1534656385652")
 .setColor('#2E86C1')
 .addField(':robot: Cyril\'s Skills Section:', 'Intro', true)
 .addField(':radio_button: Purpose:', 'Assistance and Fun.', true)
