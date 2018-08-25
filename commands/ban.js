7783a3e450f9072077c541e6de6c83f4ef80e146
Emil Barranco <41457874+EmilBInv@users.noreply.github.com>
2018-08-25 14:12:50 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,13 +3,13 @@ exports.run = (AI, message, args) => {
     const user = message.mentions.users.first();
     if (user) {
       const member = message.guild.member(user);
+      let banReason = args.join(" ").slice(22);
       if (member) {
-        let banReason = args.join(" ").slice(22);
         member.ban('Optional reason that will display in the audit logs').then(() => {
           const info = new Discord.RichEmbed()
           .setAuthor("Cyril | Mod", message.author.avatarURL)
-          .addField(`:triumph: Successfully kicked:`,  `${user}`)
-          .addField(`This User/Bot was kicked because: `, `${banReason}`)
+          .addField(`:triumph: Successfully banned:`,  `${user}`)
+          .addField(`This User/Bot was banned because: `, `${banReason}`)
           .setColor('#F39C12')
           .setTimestamp();
           message.channel.send(info)
