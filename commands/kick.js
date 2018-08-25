fec910ac8a2da27c24a6bac7085c81023d69d99d
Emil Barranco <41457874+EmilBInv@users.noreply.github.com>
2018-08-25 13:10:59 -0400

    Reason

--- commands/kick.js
+++ commands/kick.js
@@ -3,11 +3,13 @@ exports.run = (AI, message, args) => {
     const user = message.mentions.users.first();
     if (user) {
       const member = message.guild.member(user);
+    let kickReason = args.join(" ").slice(22);
     if (member) {
       member.kick('Optional reason that will display in the audit logs').then(() => {
       const info = new Discord.RichEmbed()
       .setAuthor("Cyril | Mod", "")
       .addField(`:triumph: Successfully kicked:`,  `${user}`)
+      .addField(`This User was kicked because: `, `${kickReason}`)
       .setColor('#424949')
       .setTimestamp();
       message.channel.send(info)
