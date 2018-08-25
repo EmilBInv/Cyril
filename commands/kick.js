b85ee7a470a69e7b3725c450ea8f23428501b205
Emil Barranco <41457874+EmilBInv@users.noreply.github.com>
2018-08-25 14:12:47 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -7,7 +7,7 @@ exports.run = (AI, message, args) => {
     if (member) {
       member.kick('Optional reason that will display in the audit logs').then(() => {
       const info = new Discord.RichEmbed()
-      .setAuthor("Cyril | Mod", "")
+      .setAuthor("Cyril | Mod", message.author.avatarURL)
       .addField(`:triumph: Successfully kicked:`,  `${user}`)
       .addField(`This User/Bot was kicked because: `, `${kickReason}`)
       .setColor('#F39C12')
