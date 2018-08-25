6f4018f852f5cf344830c3c43978339869c4d7a6
Emil Barranco <41457874+EmilBInv@users.noreply.github.com>
2018-08-25 14:07:10 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,7 +5,7 @@ exports.run = (AI, message, args) => {
       const member = message.guild.member(user);
       if (member) {
         let banReason = args.join(" ").slice(22);
-        member.ban().then(() => {
+        member.ban().then() => {
           const info = new Discord.RichEmbed()
           .setAuthor("Cyril | Mod", message.author.avatarURL)
           .addField(`:triumph: Successfully kicked:`,  `${user}`)
@@ -16,7 +16,7 @@ exports.run = (AI, message, args) => {
         }).catch(err => {
           message.reply('I was unable to ban the member');
           console.error(err);
-        );
+        });
       } else {
         message.reply('That user isn\'t in this guild!');
       }
